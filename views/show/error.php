f9cfe637fc5897086000129650d0d69189b735ec
Florian Bieringer <florian.bieringer@uni-passau.de>
2015-02-12 10:25:00 +0100

    Be nice

--- views/show/error.php
+++ views/show/error.php
@@ -1,1 +1,3 @@
-<?= MessageBox::error(_('Bei der Ausführung der aufgerufenen Seite ist ein Fehler aufgetreten!')) ?>
+<h1><?= _('Entschuldigung') ?></h1>
+<p><?= _('Das hätte nicht passieren dürfen, aber bei der Ausführung ist ein Fehler aufgetreten. Die Entwickler wurden informiert.') ?></p>
+<p><?= _('Bitte versuchen sie es erneut.') ?></p>
